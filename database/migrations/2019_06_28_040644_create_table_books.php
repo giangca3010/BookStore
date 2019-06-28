2848a82216994cfea24ea645f93e55c0f65e2c89
dattran0109 <thebluesnuce@gmail.com>
2019-06-28 11:36:35 +0700

    create_migration

---
+++ database/migrations/2019_06_28_040644_create_table_books.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableBooks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name_book');
+            $table->string('images');
+            $table->text('description');
+            $table->text('content');
+            $table->tinyInteger('status')->comment('1:nổi bật,2:sách mới');
+            $table->string('file');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
